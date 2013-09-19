95de3a9fa2e5cc012e0e09588d9eda466d479e6c
felipesfpaula <lipe.power@gmail.com>
2013-09-19 15:29:26 -0300

    time measure added

--- pthread/matrix_thread.c
+++ pthread/matrix_thread.c
@@ -5,18 +5,11 @@
 #include <sys/wait.h> /* for wait */
 #include <sys/shm.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include "parser.h"
 #include <time.h>
 #include <pthread.h>
 
-
-typedef struct arg_struct {
-    int *row;
-    int *column;
-    int size;
-    int element_row;
-    int element_col;
-}Args;
 
 Matrix* matrix1, *matrix2, *matrix3;
 int nroThreads;
@@ -35,17 +28,14 @@ void worker(void *arg){
 	int i,j;
 	int k = (int) arg;
 	int *row, *col;
-	//printf("Processo %d linhas ", k);
 	
-	//TODO VER POR QUE TÁ ERRADO
 	for(i=k;i<matrix1->row;i+=nroThreads){
 	row = getRowMatrix(matrix1, i);
 		for(j=0;j<matrix2->column;j++){
 		col = getColumnMatrix(matrix2,j);
 		matrix3->value[i][j] = calc_element(row,col,matrix1->row);
-		printf("[%d][%d] %d", i,j, matrix3->value[i][j]);
+		//printf("[%d][%d] %d", i,j, matrix3->value[i][j]);
 		}
-	printf("\n");
 	}
 	
 }
@@ -54,7 +44,9 @@ int main(int argc, const char *argv[]){
 
 	int i,*k;
 	pthread_t *threads;
-	Args myArg;
+	
+	//Cuidado, essa medida de tempo não é portável!
+	struct timespec ts,tf;
 	
 	nroThreads = atoi(argv[1]);
 	matrix1 = read_arquive("..//in1.txt");	
@@ -76,17 +68,21 @@ int main(int argc, const char *argv[]){
 	matrix3 = init_matrix(matrix1->row, matrix2->column);
 	
 	//matrix1->row = 10;	
-
+	
+	clock_gettime(CLOCK_MONOTONIC, &ts);
 	for(i=0;i<nroThreads;i++){
 		k[i] = i;
 		//printf("nro thread %d \n", k[i]);
 	        pthread_create(&threads[i], NULL, worker, (void *)(i));
 	}
 	
-	
 	for (i = 0; i < nroThreads; i++) 
     	  pthread_join(threads[i], NULL);
   	
-	
+  	clock_gettime(CLOCK_MONOTONIC, &tf);
+
+	 printf("TEMPO s: %d  ms: %d \n", (tf.tv_sec - ts.tv_sec) , ( ((double) (tf.tv_nsec - ts.tv_nsec)) /1000)); 
+	 
+	 write_arquive("..//out.txt",matrix3);
 	return 0;		
 }
